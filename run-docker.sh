4e1d47a815b888ec0d7af5f88338da846241adab
Alex Mendes da Costa <alexmdac@gmail.com>
2017-09-26 04:55:56 -0700

    Fix gdb breakpoints
    See https://forums.docker.com/t/gdb-breakpoints-dont-work-error-disabling-address-space-randomization/14365/2.

--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it -p 4567:4567 -v $(pwd):/work udacity/controls_kit:latest
+docker run --security-opt seccomp=unconfined -it -p 4567:4567 -v $(pwd):/work udacity/controls_kit:latest
